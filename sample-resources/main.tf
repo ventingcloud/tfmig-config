8fb529eaa95a4ec487d4198c997897cf6079b98e
Jeff McCollum <jmccollum@hashicorp.com>
2022-11-29 10:19:47 -0600

    add sample code for vcs workspaces

---
+++ sample-resources/main.tf
@@ -0,0 +1,22 @@
+variable "pet_count" {
+  type        = number
+  description = "Count of random_pet."
+  default     = 100
+}
+
+variable "length" {
+  type        = number
+  description = "Length of random_pet."
+  default     = 11
+}
+
+resource "random_pet" "main" {
+  count = var.pet_count
+
+  length    = var.length
+  separator = "-"
+
+  keepers = {
+    always = timestamp()
+  }
+}
